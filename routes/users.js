c52a3f8d281df0215dca4d08821d8898d6df3867
Tarun <tarunroy500@gmail.com>
2023-04-13 22:39:10 +0530

    feat(app.js): add dotenv package to load environment variables from .env file feat(users.js): use MONGODB_URI environment variable to connect to MongoDB database

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/cipherSchools");
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// mongoose.connect("mongodb://127.0.0.1:27017/cipherSchools");
 
 const UserSchema = new mongoose.Schema({
   firstName: String,
